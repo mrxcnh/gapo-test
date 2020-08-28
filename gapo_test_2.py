40d590f14a097cb7217a8e296a27c7e54b396389
mrxcnh <thanhdovan210184@gmail.com>
2020-08-28 21:30:49 +0700

    [ADD] fucntion test calc

---
+++ gapo_test_2.py
@@ -0,0 +1,16 @@
+def calc(formula: str, data: dict) -> float:
+    for i in data:
+        formula = formula.replace(i, str(data[i]))
+    return eval(formula)
+
+
+if __name__ == "__main__":
+    data = {
+        "price": 100,
+        "amount": 50,
+        "discount": 0.5,
+        "unit": 1000,
+        "added_tax": 50000,
+    }
+
+    print(calc("price * amount * discount", data))
